145997602242145598f428afd8f2ed382b6cc4ce
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-11-08 23:31:43 +0500

    07

--- For07.py
+++ For07.py
@@ -1,9 +1,15 @@
 def main(N):
     """
-    Return the sum of odd numbers from zero to N.
-    Args:
+     Return the sum of odd numbers from zero to N.
+   Args:
         N: int
     Returns:
         int: return  answer
     """
-    return
+    x=0
+    for i in range(N):
+        if i%2==1:
+         x+=i 
+
+    return x
+print(main(12)) 
